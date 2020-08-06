058942c04a9bfc36bdd2712fc5a15997a3a0576a
Sureyya Celep <sureyyacelep@yahoo.com>
2020-08-06 00:45:25 -0500

    update HelloB20.Java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,8 +1,9 @@
 public class HelloB20 {
     public static void main(String[] args) {
 
-        System.out.println("Hello B20, it is my pleasure to be with you!");
 
+
+        //flkjfd;l
 
 
     }
